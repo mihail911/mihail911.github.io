2855d0666db6410f201eb9a023593e4bc0114f0b
Mihail Eric <mihail911@gmail.com>
2018-09-15 23:51:32 -0700

    make dates in posts full month names

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -66,7 +66,7 @@ class PostTemplateDetails extends React.Component {
           <div className="post-single__inner">
             <h1 className="post-single__title">{post.frontmatter.title}</h1>
             <div className="post-single__date">
-              <em>{moment(post.frontmatter.date).format('MMM YYYY')}</em>
+              <em>{moment(post.frontmatter.date).format('MMMM YYYY')}</em>
             </div>
             <div className="post-single__body" dangerouslySetInnerHTML={{ __html: post.html }} />
           </div>
