4bdc0d1b3a63a6f7c47ee818952908186489807b
Mihail Eric <mihail911@gmail.com>
2018-09-22 11:29:38 -0700

    remove unused subtitle author from post template

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -7,7 +7,6 @@ import './style.scss';
 
 class PostTemplateDetails extends React.Component {
   render() {
-    const { subtitle, author } = this.props.data.site.siteMetadata;
     const post = this.props.data.markdownRemark;
     const tags = post.fields.tagSlugs;
     const relatedPosts = post.frontmatter.related;
